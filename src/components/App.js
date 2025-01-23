0e8030528c4bad74488c642d3f5f09f7bd0cdd1c
Weipin Ang <80883634+WeipinAng@users.noreply.github.com>
2025-01-23 22:41:06 +0800

    allow user to restart the quiz by resetting all the state except questions

--- src/components/App.js
+++ src/components/App.js
@@ -53,6 +53,12 @@ function reducer(state, action) {
                         ? state.points
                         : state.highscore,
             };
+        case "restart":
+            return {
+                ...initialState,
+                questions: state.questions,
+                status: STATUS_CODES.Ready,
+            };
 
         default:
             throw new Error("Action unknown");
@@ -116,6 +122,7 @@ export default function App() {
                         points={points}
                         maxPossiblePoints={maxPossiblePoints}
                         highscore={highscore}
+                        dispatch={dispatch}
                     />
                 )}
             </Main>
