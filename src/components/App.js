404e357fec97cd31c5f613ea6908512ff6e3209b
Weipin Ang <80883634+WeipinAng@users.noreply.github.com>
2025-01-23 22:13:55 +0800

    beautify the finish screen with emoji based on percentage attained

--- src/components/App.js
+++ src/components/App.js
@@ -43,6 +43,8 @@ function reducer(state, action) {
             };
         case "nextQuestion":
             return { ...state, index: state.index + 1, answer: null };
+        case "finish":
+            return { ...state, status: STATUS_CODES.Finished };
 
         default:
             throw new Error("Action unknown");
